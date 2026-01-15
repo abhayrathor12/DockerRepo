52fe892078dc8c5469816207093ca9fd8048870b
abhayrathor12 <131868927+abhayrathor12@users.noreply.github.com>
2026-01-15 12:02:14 +0530

    Change app port from 5000 to 8000port

--- DockerProject/Backend/app.py
+++ DockerProject/Backend/app.py
@@ -24,4 +24,4 @@ def submit():
 
 if __name__ == '__main__':
     # Host 0.0.0.0 is required inside Docker
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
